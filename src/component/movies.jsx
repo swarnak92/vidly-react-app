6c1a519574313478c6e09382c908184401ff825c
vikash <vikash.choudhary@diversey.com>
2018-11-27 16:06:58 +0530

    Enabled New Movie button if there is no data

--- src/component/movies.jsx
+++ src/component/movies.jsx
@@ -99,10 +99,10 @@ class Movies extends Component {
   };
 
   render() {
-    const count = this.state.movies.length;
+    // const count = this.state.movies.length;
     const { currentPage, pageSize, geners, sortColumn } = this.state;
     const { user } = this.props;
-    if (count === 0) return <p>There are no movies to display!</p>;
+    // if (count === 0) return <p>There are no movies to display!</p>;
 
     const { totalCount, data: movies, searchQuery } = this.getPagedData();
     return (
